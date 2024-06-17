11bbbb4aeed96df950bcf4232c1d153099d4775e
Eslam Juba <eslam.hussein25@gmail.com>
2024-06-17 13:30:47 +0300

    Last Edit

--- DIP.cpp
+++ DIP.cpp
@@ -76,16 +76,82 @@ class GPS
 };
 
 
+template<typename T>
+class RF
+{
+private:
+IMessage<T> * _method;
+public:
+
+void Send_Data(IMessage<T> * method , std::string msg , T Distance)
+{
+    method->SendMessage(msg , Distance);
+}
+
+
+};
+
+
+
+template<typename T>
+class Wifi
+{
+public :
+IMessage<T> * method ;
+
+void Send_Data(std::string msg , T Data)
+{
+    method->SendMessage(msg , Data);
+}
+
+};
+
+
+
 int main()
 {
     //select  the protocol to inject in your high level class 
 SPI<double> spi;
+SPI<int> spi_int;
+UART<std::string> uart_int;
 //using high level class with low level task class injection using constructor  injection 
 GPS<double> gps1(&spi) ;
 
 //send location without any care about how to send 
 gps1.Send_Location(31.2562 , 30.2506);
+
+std::cout<<"\n";
+std::cout<<"\n";
+std::cout<<"\n";
+
+//send location using bad design 
 gps1.Send_Location_with_bad_Design(31.2562 , 30.2506);
+
+
+std::cout<<"\n";
+std::cout<<"\n";
+std::cout<<"\n";
+
+//another example for injection method 
+//using method injection 
+RF<int> rf ;
+rf.Send_Data(&spi_int , "Distance: " , 130);
+
+std::cout<<"\n";
+std::cout<<"\n";
+std::cout<<"\n";
+
+
+//another example for injection method 
+//using properties injection 
+Wifi<std::string> wifi;
+//here must be select the method before send the message to avoid run time error else if we make default method in our code 
+wifi.method = &uart_int;
+wifi.Send_Data("Sensor Data:" , "H: 30 , T: 35 , NPK:{20,30,50}");
+
+
+std::cout<<"\n";
+
     return  0;
 }
 
